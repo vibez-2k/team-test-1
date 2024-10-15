f6920ec54d0f8c6e03e7cd28b5e4ea3b7eb9111b
vibez-2k <vibez2055@gmail.com>
2024-10-15 09:51:45 +0530

    initial commit

---
+++ logic.js
@@ -0,0 +1,11 @@
+function generatePassword(length) {
+    var charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=";
+    var password = "";
+    for (var i = 0; i < length; i++) {
+        var randomIndex = Math.floor(Math.random() * charSet.length);
+        password += charSet.charAt(randomIndex);
+    }
+    return password;
+}
+
+console.log(generatePassword(12));
---
+++ messages.js
@@ -0,0 +1,4 @@
+function calculate(expression) {
+    return eval(expression);
+}
+console.log(calculate("5 + 5"));
